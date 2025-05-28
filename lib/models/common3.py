ee1a8bf00443f6f61d7a208c326a7c3427672eac
huanghanyang <huanghanyang345@163.com>
2025-05-28 19:41:45 +0800

    feat:修改yolop_lane网络错误（lane_robot标签转换及griding_num参数）

--- lib/models/common3.py
+++ lib/models/common3.py
@@ -44,7 +44,7 @@ class LaneParsingHead(nn.Module):
 
     def forward(self, x):
         # x 是原始输入图像，而不是中间特征图
-        fea = x
+        # fea = x
         # print("input:", fea.shape)
         # x 是来自YOLOP骨干网络的特征图 (例如 cache[9])
 
