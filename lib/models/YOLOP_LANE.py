64ea43670b03dec8893600b8749d3ea452175225
huanghany <huanghany345@163.com>
2025-06-12 11:57:15 +0800

    feat:更新模型网络（去除车道线分割层)

--- lib/models/YOLOP_LANE.py
+++ lib/models/YOLOP_LANE.py
@@ -71,6 +71,49 @@ YOLOP_lane_robot = [
     [16, LaneParsingHead, [256, (101, 56, 2)]]  # 43: Lane pasrsing head 车道线检测头
 ]
 
+YOLOP_lane_robot_no_ll_seg = [  # 去除了车道线分割层
+    [24, 33, 34],  # Det_out_idx, Da_Segout_idx, LL_edt_idx  去除了车道线分割层
+    [-1, Focus, [3, 32, 3]],  # 0            1 12 320x320(输出尺寸）
+    [-1, Conv, [32, 64, 3, 2]],  # 1 conv    1 64 160 160
+    [-1, BottleneckCSP, [64, 64, 1]],  # 2   1 64 160 160
+    [-1, Conv, [64, 128, 3, 2]],  # 3 conv   1 128 80 80
+    [-1, BottleneckCSP, [128, 128, 3]],  # 4 1 128 80 80
+    [-1, Conv, [128, 256, 3, 2]],  # 5       1 256 40 40
+    [-1, BottleneckCSP, [256, 256, 3]],  # 6 1 256 40 40
+    [-1, Conv, [256, 512, 3, 2]],  # 7       1 512 20 20
+    [-1, SPP, [512, 512, [5, 9, 13]]],  # 8 SPP 1 512 20 20
+    [-1, BottleneckCSP, [512, 512, 1, False]],  # 9 1 512 20 20 8 10
+    [-1, Conv, [512, 256, 1, 1]],  # 10        1 256 20 20
+    [-1, Upsample, [None, 2, 'nearest']],  # 11 1 256 40 40
+    [[-1, 6], Concat, [1]],  # 12            1 512 40 40
+    [-1, BottleneckCSP, [512, 256, 1, False]],  # 13   1 256 40 40
+    [-1, Conv, [256, 128, 1, 1]],  # 14               1 128 40 40
+    [-1, Upsample, [None, 2, 'nearest']],  # 15       1 128 80 80
+    [[-1, 4], Concat, [1]],  # 16         #Encoder  1 256 80 80
+
+    [-1, BottleneckCSP, [256, 128, 1, False]],  # 17    1 128 80 80
+    [-1, Conv, [128, 128, 3, 2]],  # 18             1 128 40 40
+    [[-1, 14], Concat, [1]],  # 19                 1 256 40 40
+    [-1, BottleneckCSP, [256, 256, 1, False]],  # 20    1 256 40 40
+    [-1, Conv, [256, 256, 3, 2]],  # 21             1 256 20 20
+    [[-1, 10], Concat, [1]],  # 22                     1 512 20 20
+    [-1, BottleneckCSP, [512, 512, 1, False]],  # 23    1 512 20 20
+    [[17, 20, 23], Detect,
+     [1, [[3, 9, 5, 11, 4, 20], [7, 18, 6, 39, 12, 31], [19, 50, 38, 81, 68, 157]], [128, 256, 512]]],
+    # Detection head 24 3(20x20+40x40+80x80)  25200
+
+    [16, Conv, [256, 128, 3, 1]],  # 25              1 128 80 80
+    [-1, Upsample, [None, 2, 'nearest']],  # 26       1 128 160 160
+    [-1, BottleneckCSP, [128, 64, 1, False]],  # 27   1 64 160 160
+    [-1, Conv, [64, 32, 3, 1]],  # 28               1 32 160 160
+    [-1, Upsample, [None, 2, 'nearest']],  # 29       1 32 320 320
+    [-1, Conv, [32, 16, 3, 1]],  # 30               1 16 320 320
+    [-1, BottleneckCSP, [16, 8, 1, False]],  # 31   1 8 320 320
+    [-1, Upsample, [None, 2, 'nearest']],  # 32       1 8 640 640
+    [-1, Conv, [8, 2, 3, 1]],  # 33 Driving area segmentation head 1 2 640 640
+
+    [16, LaneParsingHead, [256, (101, 56, 2)]]  # 34: Lane pasrsing head 车道线检测头
+]
 
 class YOLOP_Lane_net(nn.Module):
     def __init__(self, block_cfg, **kwargs):
@@ -80,10 +123,11 @@ class YOLOP_Lane_net(nn.Module):
         self.detector_index = -1
         self.detector_index = block_cfg[0][0]
         self.da_seg_index = block_cfg[0][1]
-        self.ll_seg_index = block_cfg[0][2]
 
         # 检查是否存在新的车道线解析头索引
-        self.ll_parsing_index = block_cfg[0][3] if len(block_cfg[0]) > 3 else -1
+        self.ll_parsing_index = block_cfg[0][3] if len(block_cfg[0]) > 3 else block_cfg[0][2]
+
+        self.ll_seg_index = block_cfg[0][2] if len(block_cfg[0]) > 3 else -1
 
         # Build model 构建模型
         for i, (from_, block, args) in enumerate(block_cfg[1:]):
